a539b789297b5b6284ddb833c2a352d35fdd8dc2
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2023-12-14 17:34:02 +0100

    ob23:fix-fixed structure

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,24 +1,11 @@
-import ReservationForm from "../../components/molecules/ReservationForm/ReservationForm";
-import Button from "../../components/atoms/Button/Button";
+import MainSection from "../../components/organisms/MainSection/MainSection";
 import styles from "./MainPage.module.css";
 
 const MainPage = () => {
   return (
     <>
       {/* Header (OB-24 Ticket) */}
-      <section className={styles.welcomeBanner}>
-        <div className={styles.welcomeBanner_welcomeContainer}>
-          <img src="" alt="Ocean bar logo" />
-          <h1>Your Culinary Adventure Starts Here</h1>
-          <Button variant="outlined">Menu</Button>
-        </div>
-      </section>
-      <div className={styles.form_container}>
-        <ReservationForm />
-      </div>
-      <section id="menu" className={styles.menu_container}>
-        <h2>Menu</h2>
-      </section>
+      <MainSection />
       {/* Footer (OB-25 Ticket) */}
     </>
   );
