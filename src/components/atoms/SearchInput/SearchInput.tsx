be85f82dd0eecbce0355aa7baf86e1be3836b595
wrunika <2805nika@gmail.com>
2023-12-14 23:47:26 +0300

    ob24:feat-add mui library and SearchInput without logic

---
+++ src/components/atoms/SearchInput/SearchInput.tsx
@@ -0,0 +1,15 @@
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import styles from './SearchInput.module.css';
+
+export const SearchInput = () => {
+    return <TextField
+        type={"search"}
+        label={'Search...'}
+        className={styles.search}
+        InputProps={{
+            endAdornment:
+                <SearchIcon/>
+        }}
+    />
+}
