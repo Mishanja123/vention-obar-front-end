e5d6730dbfd0f0b25851efd1166704b2a82ca84d
wrunika <2805nika@gmail.com>
2023-12-14 23:50:45 +0300

    ob24:feat-create Header without search logic

---
+++ src/components/molecules/Header/Header.tsx
@@ -0,0 +1,29 @@
+import {SiIfood} from 'react-icons/si';
+import {PiShoppingCartLight} from 'react-icons/pi';
+import {GoPerson} from 'react-icons/go';
+import {IconContext} from 'react-icons';
+import styles from './Header.module.css'
+import {SearchInput} from '../../atoms/SearchInput/SearchInput';
+
+export const Header = () => {
+    return (
+        <div className={styles.headerWrapper}>
+            <a href={'/main'} className={styles.logoWrapper}>
+                OBar
+                <IconContext.Provider value={{className: styles.logo}}>
+                    <SiIfood/>
+                </IconContext.Provider>
+            </a>
+            <SearchInput/>
+            <a href={'/menu'} className={styles.menu}>Menu</a>
+            <a href={'/cart'} className={styles.reserveTable}>Reserve a table</a>
+            <a href={'/orders'} className={styles.orders}>Orders</a>
+            <IconContext.Provider value={{className: styles.cart}}>
+                <a href={'/cart'}><PiShoppingCartLight/></a>
+            </IconContext.Provider>
+            <IconContext.Provider value={{className: styles.profile}}>
+                <a href={'/profile'}><GoPerson/></a>
+            </IconContext.Provider>
+        </div>
+    )
+}
