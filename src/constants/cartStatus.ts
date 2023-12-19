f58eefea67c3028cad91329c8332cae7c245c4fb
Hennadii Borysevych <genuch3@gmail.com>
2023-12-19 11:52:40 +0100

    ob37:feat-types for entities

---
+++ src/constants/cartStatus.ts
@@ -0,0 +1,7 @@
+export enum CartStatus {
+  ACTIVE = 'Active',
+  PAID = 'Paid',
+  WILL_BE_PAID_ON_THE_SPOT = 'Will be paid on the spot',
+  COMPLETED = 'Completed',
+  CANCELED = 'Canceled',
+}
---
+++ src/constants/orderType.ts
@@ -0,0 +1,6 @@
+export enum OrderType {
+  RESERVATION = 'reservation',
+  REVERVATION_WITH_PREORDER = 'reservation_with_preorder',
+  TAKEAWAY = 'take_away',
+  DELIVERY = 'delivery',
+}
