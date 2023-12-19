10340ff13330a215a2dc678cdfc3f3e43e6ae9e4
Hennadii Borysevych <genuch3@gmail.com>
2023-12-19 13:40:31 +0100

    ob-37:feat-added cart item type

--- src/constants/orderType.ts
+++ src/constants/orderType.ts
@@ -1,5 +1,5 @@
 export enum OrderType {
-  RESERVATION = 'reservation',
+  RESERVATION_WITHOUT_PREORDER = 'reservation',
   REVERVATION_WITH_PREORDER = 'reservation_with_preorder',
   TAKEAWAY = 'take_away',
   DELIVERY = 'delivery',
--- src/types/checkoutCartItem.ts
+++ src/types/checkoutCartItem.ts
@@ -1,13 +1,10 @@
-import { CartStatus } from '@/constants/cartStatus';
-
-export type ICartItem = {
+export type ICheckoutCartItem = {
   id: string;
   title: string;
   quantity: number;
   price: number;
-  type: number;
-  status: CartStatus;
+  guests: number;
   createdAt: Date;
   reservationDate: Date;
   reservationTime: Date;
-}
+};
