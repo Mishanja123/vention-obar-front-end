8c2de9dce4d2a13a0e3534f72a3fc3a06a4bae60
Misha <shevchenkom606@gmail.com>
2023-12-21 00:59:27 +0100

    ob25:init-create some part of the footer

--- src/components/molecules/Footer/Footer.tsx
+++ src/components/molecules/Footer/Footer.tsx
@@ -1,5 +1,20 @@
+import styles from './Footer.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className={styles.contact_box}>
+        <h3 className={styles.footer_heading}>Contacts</h3>
+        <p className={styles.address}>Prosta 67, 00-838 Warszawa</p>
+        <span>Icon</span>
+        <p className={styles.phone_number}>+48 60 503 62 26 </p>
+        <span>Icon</span>
+        <p className={styles.time}>Mon - Sun 08:00 - 23:00</p>
+        <button className={styles.reservation_btn}>Make a reservation</button>
+      </div>
+      <div className={styles.map_box}>here gonna be map</div>
+    </>
+  );
 };
 
 export default Footer;
