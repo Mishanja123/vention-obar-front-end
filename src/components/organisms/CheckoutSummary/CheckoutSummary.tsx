2ded5b2eb7cbbdf1271304eacdd6188804584aa9
Hennadii Borysevych <genuch3@gmail.com>
2023-12-28 16:17:45 +0100

    ob73:feat-Navigation via all links

--- src/components/organisms/CheckoutSummary/CheckoutSummary.tsx
+++ src/components/organisms/CheckoutSummary/CheckoutSummary.tsx
@@ -3,39 +3,43 @@ import { Button } from '@/components/atoms';
 
 import styles from './CheckoutSummary.module.css';
 import { PATHS } from '@/constants/paths';
+import { Link } from 'react-router-dom';
+import useSummaryButton from '@/hooks/useSummaryButton';
 
 const CheckoutSummary = ({ path }: { path: string }) => {
-  const firstButton = path.includes(PATHS.ORDER_CONFIRMATION)
-    ? 'Confirm'
-    : path.includes(PATHS.ORDER_PAYMENT)
-      ? 'Pay  $ 200 online'
-      : 'Procced';
-
-  const secondButton = path.includes(PATHS.ORDER_CONFIRMATION)
-    ? 'Change order type'
-    : path.includes(`${PATHS.ORDER_PAYMENT}`)
-      ? 'I’ll pay on the spot'
-      : 'Cancel';
+  const {
+    firstButton,
+    secondButton,
+    firstButtonLink,
+    secondButtonLink,
+    onClick,
+  } = useSummaryButton({ path });
 
   return (
     <div className={styles.summary_section}>
       <SummaryPayment quantity={20} subtotal={50} total={500}>
         <div className={styles.summary_btn_wrapper}>
-          <Button variant="contained" type="button">
-            {firstButton}
-          </Button>
-          <Button variant="text" type="button">
-            {secondButton}
-          </Button>
-
-          {path.includes(PATHS.ORDER_PAYMENT) && (
-            <Button variant="text" type="button">
-              Back to confirmation
+          <Link to={firstButtonLink} onClick={onClick}>
+            <Button variant="contained" type="button">
+              {firstButton}
             </Button>
+          </Link>
+          <Link to={secondButtonLink} onClick={onClick}>
+            <Button variant="text" type="button">
+              {secondButton}
+            </Button>
+          </Link>
+          {path.includes(PATHS.ORDER_PAYMENT) && (
+            <Link to={PATHS.ORDER_CONFIRMATION}>
+              <Button variant="text" type="button">
+                Back to confirmation
+              </Button>
+            </Link>
           )}
         </div>
       </SummaryPayment>
     </div>
   );
 };
+
 export default CheckoutSummary;
