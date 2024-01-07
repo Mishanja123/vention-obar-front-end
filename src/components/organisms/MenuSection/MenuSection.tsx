a46faa10e14ec6727dd90d5a662a2fae8c3f5f32
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-01-07 11:19:47 +0100

    ob93:init-implemented new color styles

--- src/components/organisms/MenuSection/MenuSection.tsx
+++ src/components/organisms/MenuSection/MenuSection.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styles from './MenuSection.module.css';
 import { Outlet } from 'react-router-dom';
 import MenuNavigator from '@/components/molecules/MenuNavigator/MenuNavigator';
-import Pagination from '@/components/atoms/Pagination/Pagination';
-import { useMenuContext } from '@/context/menuContext';
 
 type MenuSectionProps = object;
 
