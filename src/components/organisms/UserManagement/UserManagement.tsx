bfcdb906bf29b7c117a56497ff0b1b83d6b80c32
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-01-11 12:31:49 +0100

    ob103:feat-added user modification feature

--- src/components/organisms/UserManagement/UserManagement.tsx
+++ src/components/organisms/UserManagement/UserManagement.tsx
@@ -12,8 +12,23 @@ interface User {
   UserCreditCardId?: number | null;
 }
 
+interface EditedUser {
+  id?: number; // Assuming you don't want to edit the ID
+  first_name: string;
+  last_name: string;
+  email: string;
+  UserAddressId?: number | null;
+  UserCreditCardId?: number | null;
+}
+
 const UserManagement: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [editingModeIndex, setEditingModeIndex] = useState<number>(-1);
+  const [editedUser, setEditedUser] = useState<EditedUser>({
+    first_name: '',
+    last_name: '',
+    email: '',
+  });
 
   const fetchUsers = async () => {
     try {
@@ -25,10 +40,36 @@ const UserManagement: React.FC = () => {
       console.log('Ooops, looks like there is an error ' + error);
     }
   };
-
   useEffect(() => {
     fetchUsers();
   }, []);
+  const handleEditMode = (index: number) => {
+    if (editingModeIndex === index) {
+      setEditingModeIndex(-1);
+    } else {
+      setEditingModeIndex(index);
+      setEditedUser(users[index]);
+    }
+  };
+
+  const handleSaveUser = async () => {
+    try {
+      const response = await axiosInstance.patch(
+        `/users/${editedUser.id}`,
+        editedUser,
+      );
+
+      const updatedUser: User = response.data;
+      console.log(updatedUser);
+      setUsers((prev) =>
+        prev.map((user) => (user.id === updatedUser.id ? updatedUser : user)),
+      );
+
+      setEditingModeIndex(-1);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <table className={styles.users_table}>
@@ -47,13 +88,59 @@ const UserManagement: React.FC = () => {
         {users.map((user, index) => (
           <tr key={index}>
             <td>{user.id}</td>
-            <td>{user.first_name}</td>
-            <td>{user.last_name}</td>
-            <td>{user.email}</td>
+            <td>
+              {editingModeIndex === index ? (
+                <input
+                  type="text"
+                  value={editedUser.first_name}
+                  onChange={(e) =>
+                    setEditedUser({ ...editedUser, first_name: e.target.value })
+                  }
+                />
+              ) : (
+                user.first_name
+              )}
+            </td>
+            <td>
+              {editingModeIndex === index ? (
+                <input
+                  type="text"
+                  value={editedUser.last_name}
+                  onChange={(e) =>
+                    setEditedUser({ ...editedUser, last_name: e.target.value })
+                  }
+                />
+              ) : (
+                user.last_name
+              )}
+            </td>
+            <td>
+              {editingModeIndex === index ? (
+                <input
+                  type="text"
+                  value={user.email}
+                  onChange={(e) =>
+                    setEditedUser({ ...editedUser, email: e.target.value })
+                  }
+                />
+              ) : (
+                user.email
+              )}
+            </td>
             <td>{user.UserAddressId ? 'null' : 'None'}</td>
             <td>{user.UserCreditCardId ? 'null' : 'None'}</td>
             <td>
-              <Button variant="outlined">Edit</Button>
+              {editingModeIndex === index ? (
+                <Button variant="outlined" onClick={() => handleSaveUser()}>
+                  Save
+                </Button>
+              ) : (
+                <Button
+                  variant="outlined"
+                  onClick={() => handleEditMode(index)}>
+                  Edit
+                </Button>
+              )}
             </td>
           </tr>
         ))}
