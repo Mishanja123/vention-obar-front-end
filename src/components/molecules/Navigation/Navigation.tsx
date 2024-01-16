179f298cd6fb1def01bdca55d03f55112ca2ef9a
Misha <shevchenkom606@gmail.com>
2024-01-16 01:39:04 +0100

    ob125:fix-fix navigation component

--- src/components/molecules/Navigation/Navigation.tsx
+++ src/components/molecules/Navigation/Navigation.tsx
@@ -1,61 +1,39 @@
-import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
+
 import { IconContext } from 'react-icons';
 import { PiShoppingCartLight } from 'react-icons/pi';
 import { GoPerson } from 'react-icons/go';
 
 import { PATHS } from '@/constants/paths';
 import { useAuthContext } from '@/context/authContext';
+import { scrollToReservationForm } from '@/helpers';
+import { showReservationModal } from '@/helpers';
 
-import { ReservationForm } from '@/components/molecules';
 import { LinkWrapper } from '@/components/atoms/index.ts';
 import { Button } from '@/components/atoms/index.ts';
-import { scrollToReservationForm } from '@/helpers/scrollToReservation.ts';
 
 import styles from './Navigation.module.css';
 
-const Navigation = () => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+type Props = {
+  loc: string;
+};
 
+const Navigation: React.FC<Props> = ({ loc }) => {
   const location = useLocation();
   const path = location.pathname;
-  const mySwal = withReactContent(Swal);
 
   const { logOut } = useAuthContext();
 
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const showReservationModal = () =>
-    mySwal.fire({
-      title: 'Make a reservation',
-      html: <ReservationForm />,
-      confirmButtonText: 'Close',
-    });
-
   return (
-    <>
+    <div className={`${styles[loc]}`}>
       <LinkWrapper to={PATHS.MENU}>Menu</LinkWrapper>
-      {windowWidth >= 876 && (
-        <Button
-          variant="contained"
-          onClick={
-            path === PATHS.ROOT ? scrollToReservationForm : showReservationModal
-          }>
-          Reserve a table
-        </Button>
-      )}
+      <Button
+        variant="contained"
+        onClick={
+          path === PATHS.ROOT ? scrollToReservationForm : showReservationModal
+        }>
+        Reserve a table
+      </Button>
       <LinkWrapper to={PATHS.ORDERS}>Orders</LinkWrapper>
       <LinkWrapper to={PATHS.CART}>
         <IconContext.Provider value={{ className: styles.cart_img }}>
@@ -67,12 +45,10 @@ const Navigation = () => {
           <GoPerson />
         </IconContext.Provider>
       </LinkWrapper>
-      {windowWidth >= 876 && (
-        <Button variant="contained" onClick={() => logOut()}>
-          Log out
-        </Button>
-      )}
-    </>
+      <Button variant="contained" onClick={() => logOut()}>
+        Log out
+      </Button>
+    </div>
   );
 };
 
