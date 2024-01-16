146b0fe381a2bdb2b72aebe871788ca3ae75aa76
Misha <shevchenkom606@gmail.com>
2024-01-16 14:16:49 +0100

    ob122:fix-fix cart styles

--- src/components/molecules/CartItem/CartItem.tsx
+++ src/components/molecules/CartItem/CartItem.tsx
@@ -2,15 +2,16 @@ import QuantityCounter from '../QuantityCounter/QuantityCounter';
 import { IoTrash } from 'react-icons/io5';
 
 import styles from './CartItem.module.css';
-import { CartItem } from '@/models/cart.model';
+import { IconContext } from 'react-icons';
+// import { CartItem } from '@/models/cart.model';
 
 type CartItemProps = {
   imageURL: string;
   title: string;
   price: number;
   quantity: number;
-  addToCart: (item: CartItem) => void;
-  removeFromCart: (item: CartItem) => void;
+  // addToCart: (item: CartItem) => void;
+  // removeFromCart: (item: CartItem) => void;
 };
 
 const CartItems = ({
@@ -18,26 +19,28 @@ const CartItems = ({
   title,
   price,
   quantity,
-  addToCart,
-  removeFromCart,
+  // addToCart,
+  // removeFromCart,
 }: CartItemProps) => {
   return (
     <li className={styles.cart_item_wrapper}>
-      <svg width="200" height="200" className={styles.cart_item_image}>
-        <use href={`${imageURL}#icon-avatar`} aria-expanded="true"></use>
-      </svg>
-      <div>
+      <div className={styles.img_wrapper}>
+        <svg width="200" height="200" className={styles.cart_item_image}>
+          <use href={`${imageURL}#icon-avatar`} aria-expanded="true"></use>
+        </svg>
         <div>
           <h3 className={styles.cart_item_title}>{title}</h3>
-          <p className={styles.cart_item_price}>{price}</p>
+          <p className={styles.cart_item_price}>{price}$</p>
         </div>
       </div>
       <QuantityCounter
         quantity={quantity}
-        addToCart={addToCart}
-        removeFromCart={removeFromCart}
+        // addToCart={addToCart}
+        // removeFromCart={removeFromCart}
       />
-      <IoTrash className={styles.trash_icon} />
+      <IconContext.Provider value={{ className: styles.trash_icon }}>
+        <IoTrash />
+      </IconContext.Provider>
     </li>
   );
 };
