49abca09e7ca8c32cad03e69016ee20f843bcf64
Misha <shevchenkom606@gmail.com>
2024-01-18 22:24:03 +0100

    ob132:fix-deleted files associated with the context

--- src/router/mainRouter.tsx
+++ src/router/mainRouter.tsx
@@ -15,6 +15,7 @@ import MenuPage from '../pages/MenuPage';
 import { PATHS } from '../constants/paths';
 
 import PrivatePage from '../routes/PrivateRoute';
+
 import CheckoutPage from '../pages/CheckoutPage/CheckoutPage';
 import OrdersPage from '../pages/OrdersPage/OrdersPage';
 import {
@@ -31,9 +32,9 @@ const mainRoutes = [
   {
     path: PATHS.ROOT,
     element: (
-      // <PrivatePage>
-      <SharedLayout />
-      // </PrivatePage>
+      <PrivatePage>
+        <SharedLayout />
+      </PrivatePage>
     ),
     children: [
       { path: PATHS.ROOT, element: <MainPage /> },
