3786730c54060fb02ec7f7af915cbe11b4846806
Misha <shevchenkom606@gmail.com>
2024-01-18 22:43:01 +0100

    ob132:fix-conflict

--- src/services/restaurantAPI.ts
+++ src/services/restaurantAPI.ts
@@ -8,34 +8,4 @@ export const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-export const setAccessToken = (accessToken: string) => {
-  axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-};
-
-export const unsetAccessToken = () => {
-  axiosInstance.defaults.headers.common.Authorization = '';
-};
-
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response.status == 401) {
-      try {
-        const { headers } = await axiosInstance.get('/auth/refresh');
-        const newAccessToken = headers.authorization.split(' ')[1];
-
-        setAccessToken(newAccessToken);
-
-        localStorage.setItem('token', JSON.stringify(newAccessToken));
-
-        return await axiosInstance(error.config);
-        // eslint-disable-next-line @typescript-eslint/no-shadow
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    }
-    return Promise.reject(error);
-  },
-);
-
 export default axiosInstance;
