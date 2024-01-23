2cebc274969757c18973bea1ffe99cc55680fbad
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-01-23 11:39:11 +0100

    ob136-implemented initial user data on the account page FE part

--- src/components/molecules/UserInfoForm/UserInfoForm.tsx
+++ src/components/molecules/UserInfoForm/UserInfoForm.tsx
@@ -7,8 +7,13 @@ import sprite from '@/assets/sprite.svg';
 import { userInfoFormInputs } from '@/content/accountForms/userInfoFormInputs';
 import { userFormSchema } from '@/validationSchemas/userFormSchema';
 import { TextInput } from '@/components/atoms';
+import { useEffect, useState } from 'react';
+import { getUserInfo } from './userInfo';
+import axiosInstance from '@/services/restaurantAPI';
 
 const UserInfoForm = () => {
+  const [editMode, setEditMode] = useState(false);
+  const [userId, setUserId] = useState<number>();
   const formik = useFormik({
     initialValues: {
       firstName: '',
@@ -18,7 +23,7 @@ const UserInfoForm = () => {
       password: '',
     },
     validationSchema: userFormSchema,
-    onSubmit: ({
+    onSubmit: async ({
       firstName,
       lastName,
       email,
@@ -26,8 +31,40 @@ const UserInfoForm = () => {
       password,
     }: FormikValues) => {
       console.log(firstName, lastName, email, phone, password);
+      try {
+        const response = await axiosInstance.patch(`/users/${userId}`, {
+          first_name: firstName,
+          lastName: lastName,
+          email: email,
+          phone: phone,
+        });
+        const updatedUser = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+      setEditMode(false);
     },
   });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userData = await getUserInfo();
+        const userInformation = userData?.user;
+        setUserId(userInformation?.id!);
+        formik.setValues({
+          firstName: userInformation?.first_name || '',
+          lastName: userInformation?.last_name || '',
+          email: userInformation?.email || '',
+          phone: userInformation?.phone || '',
+          password: '',
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.user_info_section}>
       <div className={styles.user_image_wrapper}>
@@ -36,15 +73,29 @@ const UserInfoForm = () => {
         </svg>
         <button className={styles.image_change_button}>Change</button>
       </div>
-      <form className={styles.user_info_form} onSubmit={formik.handleSubmit}>
+      <form
+        className={styles.user_info_form}
+        onSubmit={(e) => {
+          e.preventDefault();
+          formik.handleSubmit(e);
+        }}>
         {userInfoFormInputs.map((input, i) => (
           <label htmlFor={input.name} key={i}>
-            <TextInput {...input} formik={formik} />
+            <TextInput {...input} formik={formik} disabled={!editMode} />
           </label>
         ))}
-        <button type="submit" className={styles.image_change_button}>
-          Edit/Save changes
-        </button>
+        {editMode ? (
+          <button type="submit" className={styles.image_change_button}>
+            Save
+          </button>
+        ) : (
+          <button
+            type="button"
+            className={styles.image_change_button}
+            onClick={() => setEditMode(true)}>
+            Edit
+          </button>
+        )}
       </form>
     </div>
   );
