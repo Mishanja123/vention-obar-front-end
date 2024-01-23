77b9bfcbdfc0c8d9add07f9421404e9331648cde
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-01-23 18:11:53 +0100

    ob146:init-initialized skeleton implementation

--- src/components/organisms/AccountPageSection/AccountPageSection.tsx
+++ src/components/organisms/AccountPageSection/AccountPageSection.tsx
@@ -1,12 +1,13 @@
 import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './AccountPageSection.module.css';
 import { PATHS } from '@/constants/paths';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { Skeleton } from '@mui/material';
 
 const AccountPageSection = () => {
   const location = useLocation();
   const navigate = useNavigate();
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     if (location.pathname === PATHS.ACCOUNT) {
       navigate(PATHS.USER_INFO);
@@ -18,26 +19,83 @@ const AccountPageSection = () => {
     color: isActive ? '#daa520' : '',
   });
 
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2000);
+
   return (
     <section className={styles.account_section}>
-      <ul className={styles.account_list}>
-        <li className={styles.account_item}>
-          <NavLink to={PATHS.USER_INFO} style={navLinkStyle}>
-            Personal Data
-          </NavLink>
-        </li>
-        <li className={styles.account_item}>
-          <NavLink to={PATHS.DELIVERY_ADDRESS} style={navLinkStyle}>
-            Delivery address
-          </NavLink>
-        </li>
-        <li className={styles.account_item}>
-          <NavLink to={PATHS.PAYMENT} style={navLinkStyle}>
-            Payments
-          </NavLink>
-        </li>
-      </ul>
-      <Outlet />
+      {isLoading ? (
+        <div className={styles.skeleton_container}>
+          <div className={styles.account_navigation}>
+            <Skeleton
+              animation="wave"
+              variant="rounded"
+              width={210}
+              className={styles.skeleton_navigation_tab}
+              height={80}
+            />
+            <Skeleton
+              animation="wave"
+              className={styles.skeleton_navigation_tab}
+              variant="rounded"
+              width={210}
+              height={80}
+            />
+            <Skeleton
+              animation="wave"
+              variant="rounded"
+              className={styles.skeleton_navigation_tab}
+              width={210}
+              height={80}
+            />
+          </div>
+          <div className={styles.user_profilePhoto}>
+            <Skeleton
+              animation="wave"
+              variant="circular"
+              width={150}
+              height={150}
+            />
+            <Skeleton
+              animation="wave"
+              variant="rounded"
+              width={150}
+              height={50}
+            />
+          </div>
+          <div className={styles.user_profile_form}>
+            <Skeleton
+              animation="wave"
+              variant="rounded"
+              className={styles.skeleton_profile_form}
+              width={300}
+              height={400}
+            />
+          </div>
+        </div>
+      ) : (
+        <>
+          <ul className={styles.account_list}>
+            <li className={styles.account_item}>
+              <NavLink to={PATHS.USER_INFO} style={navLinkStyle}>
+                Personal Data
+              </NavLink>
+            </li>
+            <li className={styles.account_item}>
+              <NavLink to={PATHS.DELIVERY_ADDRESS} style={navLinkStyle}>
+                Delivery address
+              </NavLink>
+            </li>
+            <li className={styles.account_item}>
+              <NavLink to={PATHS.PAYMENT} style={navLinkStyle}>
+                Payments
+              </NavLink>
+            </li>
+          </ul>
+          <Outlet />
+        </>
+      )}
     </section>
   );
 };
