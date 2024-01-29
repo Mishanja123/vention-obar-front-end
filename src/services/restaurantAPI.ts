670551f1b1fd27bbdea50419ea35b14e383f879e
Misha <shevchenkom606@gmail.com>
2024-01-29 12:12:17 +0100

    ob165:fix-port

--- src/services/restaurantAPI.ts
+++ src/services/restaurantAPI.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-// const HOST = import.meta.env.BASE_URL;
+const HOST = import.meta.env.BASE_URL;
 export const axiosInstance = axios.create({
-  // baseURL: `http://${HOST}:3000/api`,
-  baseURL: `http://18.192.215.237:3000/api`,
+  baseURL: `http://${HOST}:3000/api`,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
