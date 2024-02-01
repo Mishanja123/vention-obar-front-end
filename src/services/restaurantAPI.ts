e81c971658eaa47345865b0b0b83250b170c6968
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-02-01 22:39:34 +0100

    ob183-resolved merge conflicts

--- src/services/restaurantAPI.ts
+++ src/services/restaurantAPI.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-// const HOST = import.meta.env.BASE_URL;
+const HOST = process.env.BASE_URL;
+
+console.log(HOST);
 
 export const axiosInstance = axios.create({
-  baseURL: `http://localhost:3000/api`,
+  baseURL: `http://${HOST}:3000/api`,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
