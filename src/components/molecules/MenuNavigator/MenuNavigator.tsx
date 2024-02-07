eaaca72978633a95ede010b20f58f59800961cb0
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-02-07 11:33:16 +0100

    ob200:init-implemented fix for dish details navigation bug

--- src/components/molecules/MenuNavigator/MenuNavigator.tsx
+++ src/components/molecules/MenuNavigator/MenuNavigator.tsx
@@ -3,17 +3,23 @@ import styles from './MenuNavigator.module.css';
 import { useMenuContext } from '@/context/menuContext';
 import { DISHCATEGORY } from '@/constants/categoryDish';
 import { handleTitleNormalized } from '@/helpers';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { PATHS } from '@/constants/paths';
 
 const MenuNavigator = () => {
   const [activeCategory, setActiveCategory] = useState<DISHCATEGORY | null>(
     null,
   );
-
+  const location = useLocation();
+  const navigate = useNavigate();
   const { setCategory } = useMenuContext();
+
+  const MENU_PATH = PATHS.MENU;
 
   const handleCategoryClick = (category: DISHCATEGORY) => {
     setActiveCategory(category);
     setCategory(category);
+    if (location.pathname !== MENU_PATH) navigate(MENU_PATH);
   };
 
   return (
