0d928798f361229dd186dc0ee95aabdd5d3c27d7
Rostyslav Bezverkhnii <bezverkhniii@gmail.com>
2024-02-12 12:50:16 +0000

    Update s3axiosInstance.ts

--- src/services/s3axiosInstance.ts
+++ src/services/s3axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const HOST = import.meta.env.VITE_BACKEND_HOST; //create env file with this naming
 
-const HOST = '54.93.202.73';
+const HOST = '3.74.162.254';
 
 export const s3axiosInstance = axios.create({
   baseURL: `http://${HOST}:3000/api`,
