ce5ef79e5e2598defb732939866cf9d3f45e5119
Rostyslav Bezverkhnii <bezverkhnii@Rostyslavs-MacBook-Air.local>
2024-02-13 15:00:52 +0100

    changed vercels config

--- src/services/s3axiosInstance.ts
+++ src/services/s3axiosInstance.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 const HOST = '54.93.51.4';
 
 export const s3axiosInstance = axios.create({
-  baseURL: `https://${HOST}:3000/api`,
+  baseURL: `http://${HOST}:3000/api`,
   headers: { 'Content-Type': 'multipart/form-data' },
   withCredentials: true,
 });
